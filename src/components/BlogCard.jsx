8caf530ebca71d3dfd7a0557ccdd3890e45c7302
Dano <dlg98@gmail.com>
2023-05-09 12:18:22 -0800

    style: Add styles and animations to blog card

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,17 +1,27 @@
 import { Link } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
 
 export default function BlogCard({ img, alt, title, paragraph }) {
+  const { ref, inView } = useInView({
+    threshold: 0.1,
+  });
   return (
-    <article className="w-full sm:w-80 h-auto rounded-lg bg-lime-200/100 border border-green-950 flex flex-col items-center justify-center">
+    <article
+      ref={ref}
+      className={`w-full sm:w-80 h-auto rounded-lg shadow transition-all shadow-green-950 bg-lime-200/100 border border-green-950 flex flex-col items-center justify-center ${
+        inView ? `animate-in zoom-in-0 delay-100 duration-500` : "scale-0"
+      }`}
+    >
       <img
         src={img}
         alt={alt}
+        loading="lazy"
         className="w-full h-48 rounded-tr-md rounded-tl-md"
       />
       <div className="w-full h-1/2 p-3 flex flex-col items-center justify-evenly gap-y-4">
-        <strong className="text-center text-lg text-green-950">{title}</strong>
+        <strong className="text-center text-xl text-green-950">{title}</strong>
         <h4 className="text-center  line-clamp-3">{paragraph}</h4>
-        <Link className="bg-green-950 text-green-100 px-7 py-3 rounded-md">
+        <Link className="bg-green-950 hover:bg-green-100 text-green-100 hover:text-green-950 border border-green-950 px-7 py-3 rounded-md font-semibold">
           Leer mas
         </Link>
       </div>
